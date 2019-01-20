baadd924bec3a3307bc4fffdbb4e92e6ca943e0a
ZimnyKotlet <zadruzynskimateusz@gmail.com>
2019-01-20 15:58:51 +0100

    Add existing file

---
+++ dodawanie1.cpp
@@ -0,0 +1,20 @@
+#include 
+
+using namespace std;
+
+int main()
+
+{
+
+int a,b,c;
+
+cout << "Podaj a: "<
+cin >>a;
+
+cout << "Podaj b: "<
+cin >>b;
+
+c=a+b;
+
+cout << "Suma dwu liczb to " << c<
+}
