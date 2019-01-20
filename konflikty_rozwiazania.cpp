37beff918fb800573b47f1d1861914286eb65962
adibuu <adibu8@gmail.com>
2019-01-20 17:31:02 +0100

    konflikty rozwiazanie

---
+++ konflikty_rozwiazania.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int a,b;
+
+cout<<"Podaj a: ";
+cin>>a;
+
+cout<<"Podaj b: ";
+cin>>b;
+
+cout<<"Wynik: "<<a-b;
+
+system ("pause");
+return 0;
+}
