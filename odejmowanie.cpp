54d90eaa87e4606da38bad66549f4374a4525cac
Maciej <mocador23@o2.pl>
2019-01-20 16:57:52 +0100

    blabla

---
+++ odejmowanie.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+
+using namespace std;
+
+
+int main() {
+	int a, b;
+	cout << "Odejmowanie (a-b)";
+	cout << endl;
+	cout << "Podaj a: ";
+	cin >> a;
+	cout << "Podaj b: ";
+	cin >> b;
+	int wynik;
+	wynik = a - b;
+	cout << "Wynik to: " << wynik;
+	_getch();
+
+
+
+
+
+
+
+
+}
