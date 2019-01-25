15e8bbf013eebc8c3e99bf5d8952089aa8d8dd62
OdgrzewanyKotlet <mocador23@o2.pl>
2019-01-25 13:14:37 +0100

    Dopisalem zdanie testowe programu.

---
+++ nowyplik.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+
+int main(){
+
+
+cout<<"Program testowy";
+cout<<"Nacisnij dowolny przycisk aby zakonczyc...";
+
+_getch();
+return 0;
+
+
+}
